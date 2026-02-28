e4c5a6e5610bc21c556f67957e93c70eee3871b5
Michael Bestas <mkbestas@lineageos.org>
2026-02-28 23:42:10 +0200

    ares: Patch some blobs to depend on libtinyxml2-v34.so
    * libtinyxml2 10.1.0 update which landed in BP4A contains ABI
      incompatible changes which cause runtime crashes.
    
    Change-Id: I7f467ff313eef121cc35a2f28de60e841001e785

--- extract-files.py
+++ extract-files.py
@@ -30,6 +30,7 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib/hw/audio.primary.mt6893.so': blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautils-v30.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
         .add_needed('libstagefright_foundation-v33.so'),
     ('vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so', 'vendor/lib64/libmtkcam_stdutils.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
